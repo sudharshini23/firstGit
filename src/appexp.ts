46b35f10926a039f2723f338e3add637e5fad5b6
sudharshini23 <sudarsh25@gmail.com>
2023-08-02 16:43:11 +0530

    Typescript Files

---
+++ src/appexp.ts
@@ -0,0 +1,13 @@
+import express = require("express")
+
+import bodyParser = require('body-parser'); 
+
+import todosRoutes from './routes/todos';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosRoutes)
+
+app.listen(3000);
